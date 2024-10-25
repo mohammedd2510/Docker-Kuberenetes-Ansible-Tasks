b34d8b8f14c868d3454db4896b983bc78fd8d825
Mohamed Osama <mohammad.osamaa25@gmail.com>
2024-10-25 21:46:19 +0300

    Docker Tasks Added

--- Docker_Tasks/Docker_Compose_Task/app.py
+++ Docker_Tasks/Docker_Compose_Task/app.py
@@ -20,4 +20,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! from mohamed I have been seen {} times.\n'.format(count)
+    return 'Hello World! from mohamedosama I have been seen {} times.\n'.format(count)
